4aff2cfd5961567bc326a4cef960d1a426c8c0fc
Jongwoo Han <jongwooo.han@gmail.com>
2024-10-20 20:43:01 +0900

    Refactor Builder classes for request objects
    Signed-off-by: Jongwoo Han <jongwooo.han@gmail.com>

--- src/main/java/io/litmuschaos/request/ProjectNameRequest.java
+++ src/main/java/io/litmuschaos/request/ProjectNameRequest.java
@@ -1,36 +1,15 @@
 package io.litmuschaos.request;
 
+import io.litmuschaos.util.Builder;
+
 public class ProjectNameRequest {
 
     private final String projectID;
     private final String projectName;
 
-    private ProjectNameRequest(Builder builder) {
+    private ProjectNameRequest(ProjectNameRequestBuilder builder) {
         this.projectID = builder.projectID;
         this.projectName = builder.projectName;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private String projectID;
-        private String projectName;
-
-        public Builder projectID(String projectID) {
-            this.projectID = projectID;
-            return this;
-        }
-
-        public Builder projectName(String projectName) {
-            this.projectName = projectName;
-            return this;
-        }
-
-        public ProjectNameRequest build() {
-            return new ProjectNameRequest(this);
-        }
     }
 
     public String getProjectID() {
@@ -39,5 +18,30 @@ public class ProjectNameRequest {
 
     public String getProjectName() {
         return projectName;
+    }
+
+    public static ProjectNameRequestBuilder builder() {
+        return new ProjectNameRequestBuilder();
+    }
+
+    public static class ProjectNameRequestBuilder implements Builder<ProjectNameRequest> {
+
+        private String projectID;
+        private String projectName;
+
+        public ProjectNameRequestBuilder projectID(String projectID) {
+            this.projectID = projectID;
+            return this;
+        }
+
+        public ProjectNameRequestBuilder projectName(String projectName) {
+            this.projectName = projectName;
+            return this;
+        }
+
+        @Override
+        public ProjectNameRequest build() {
+            return new ProjectNameRequest(this);
+        }
     }
 }
