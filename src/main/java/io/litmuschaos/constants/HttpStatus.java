3430d605501de7d2bbb190f6d0744577d9a73253
Jemin <jemin9812@gmail.com>
2025-02-03 03:14:17 +0900

    feat: add http status constant
    Signed-off-by: Jemin <jemin9812@gmail.com>

---
+++ src/main/java/io/litmuschaos/constants/HttpStatus.java
@@ -0,0 +1,9 @@
+package io.litmuschaos.constants;
+
+public class HttpStatus {
+    public static final int OK = 200;
+    public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
+    public static final int NOT_FOUND = 404;
+}
